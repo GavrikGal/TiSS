0d9175615b549f09c3e78892b057a76433b98b2f
GavrikGal <gavrik.gal@gmail.com>
2023-09-12 16:25:41 +0300

    TISS-28 Плоттер доверительных интервалов. Одиночные графики и по частотам

--- plotter/base_handler.py
+++ plotter/base_handler.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Dict, List
 
 from plotter.base_plotter import BasePlotter
 from plotter.style.grid_style import GridStyle, default_major_grid, default_minor_grid
@@ -8,8 +9,8 @@ from plotter.style.line_style import LineStyle, default_line_style_set
 class BaseHandler(ABC):
     """Абстракция обработчика построителя графиков"""
 
-    default_grid_styles: GridStyle = {'major': default_major_grid, 'minor': default_minor_grid}
-    default_line_styles: LineStyle = default_line_style_set
+    grid_styles: Dict[str, GridStyle] = {'major': default_major_grid, 'minor': default_minor_grid}
+    line_styles: List[LineStyle] = default_line_style_set
 
     @abstractmethod
     def plot(self, plotter: BasePlotter) -> None:
--- plotter/plt/error_bar.py
+++ plotter/plt/error_bar.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as st
+from typing import Dict, List
 
 from plotter.base_handler import BaseHandler
 from plotter.base_plotter import BasePlotter
+from plotter.style.grid_style import GridStyle
+from plotter.style.line_style import LineStyle
 
 
 class ErrorBarHandler(BaseHandler):
     """Обработчик построения графиков доверительных интервалов"""
+    def __init__(self, grid_styles: Dict[str, GridStyle] = None,
+                 line_styles: List[LineStyle] = None):
+        if grid_styles:
+            self.grid_styles = grid_styles
+        if line_styles:
+            self.line_styles = line_styles
+        else:
+            self.line_styles = [LineStyle('black', '-', 1)]
 
     def plot(self, plotter: BasePlotter) -> None:
 
@@ -16,7 +27,6 @@ class ErrorBarHandler(BaseHandler):
 
         plt.figure(layout='constrained',
                    figsize=(plotter.get_w_plot(), plotter.get_h_plot()))
-
         subplot_names = plotter.get_subplot_names()
 
         for i_sub, subplot_name in enumerate(subplot_names):
@@ -34,7 +44,9 @@ class ErrorBarHandler(BaseHandler):
                     y.append(df[subplot_name].mean())
                     data = df[subplot_name].dropna().values
                     yerr.append(np.mean(data) - st.t.interval(confidence=0.95, df=len(data) - 1, loc=np.mean(data),
-                                                              scale=st.sem(data))[0])
+                                                              scale=st.tstd(data))[0])
             axes.errorbar(x=x, y=y, yerr=yerr,
-                          color="black", capsize=5, marker="o",
-                          markersize=8, mfc="red", mec="black")
+                          color=self.line_styles[0].color, capsize=5, marker="o",
+                          markersize=8, mfc="red", mec=self.line_styles[0].color)
+
+            axes.set_yticks(np.arange(0, self.grid_styles.get('major').max_y_tick, 10))
